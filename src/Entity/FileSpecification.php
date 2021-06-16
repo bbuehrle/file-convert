188f484758988c936768ce92b66b9d7e4cb9044a
Brian Buehrle <bbueh02@outlook.com>
2021-06-16 17:34:56 -0500

    Make FileSpecification->name be unique

--- src/Entity/FileSpecification.php
+++ src/Entity/FileSpecification.php
@@ -7,10 +7,12 @@ use App\Repository\FileSpecificationRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ApiResource()
  * @ORM\Entity(repositoryClass=FileSpecificationRepository::class)
+ * @UniqueEntity(fields={"name"})
  */
 class FileSpecification
 {
@@ -22,7 +24,7 @@ class FileSpecification
     private ?int $id = null;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private ?string $name = null;
 
